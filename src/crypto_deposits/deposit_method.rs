44b8c6e87f2c87898161a5e53f6415fd4339d935
amigin <amigin@gmail.com>
2024-09-28 10:56:24 +0300

    Small Fixes

--- src/crypto_deposits/deposit_method.rs
+++ src/crypto_deposits/deposit_method.rs
@@ -1,38 +1,38 @@
 #[derive(Debug, Clone, Copy)]
-pub enum DepositMethod {
+pub enum CryptoDepositMethod {
     Native,
     EthErc20,
     Trc20,
     BinanceBep20,
 }
 
-impl DepositMethod {
+impl CryptoDepositMethod {
     pub fn from_str(s: &str) -> Option<Self> {
         match s {
-            "Native" => Some(DepositMethod::Native),
-            "EthErc20" => Some(DepositMethod::EthErc20),
-            "Trc20" => Some(DepositMethod::Trc20),
-            "BinanceBep20" => Some(DepositMethod::BinanceBep20),
+            "" => Some(CryptoDepositMethod::Native),
+            "EthErc20" => Some(CryptoDepositMethod::EthErc20),
+            "Trc20" => Some(CryptoDepositMethod::Trc20),
+            "BinanceBep20" => Some(CryptoDepositMethod::BinanceBep20),
             _ => None,
         }
     }
 
     pub fn as_str(&self) -> &str {
         match self {
-            DepositMethod::Native => "Native",
-            DepositMethod::EthErc20 => "EthErc20",
-            DepositMethod::Trc20 => "Trc20",
-            DepositMethod::BinanceBep20 => "BinanceBep20",
+            CryptoDepositMethod::Native => "",
+            CryptoDepositMethod::EthErc20 => "EthErc20",
+            CryptoDepositMethod::Trc20 => "Trc20",
+            CryptoDepositMethod::BinanceBep20 => "BinanceBep20",
         }
     }
 
     pub fn is_native(&self) -> bool {
-        matches!(self, DepositMethod::Native)
+        matches!(self, CryptoDepositMethod::Native)
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct AssetDepositMethod {
+pub struct AssetCryptoDepositMethod {
     pub asset_id: &'static str,
-    pub deposit_methods: Vec<DepositMethod>,
+    pub deposit_methods: Vec<CryptoDepositMethod>,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,85 +1,85 @@
-use crypto_deposits::{AssetDepositMethod, DepositMethod};
+use crypto_deposits::{AssetCryptoDepositMethod, CryptoDepositMethod};
 
 pub mod crypto_deposits;
 
 lazy_static::lazy_static! {
-    pub static ref ASSET_DEPOSIT_METHODS: Vec<AssetDepositMethod>  = {
+    pub static ref ASSET_DEPOSIT_METHODS: Vec<AssetCryptoDepositMethod>  = {
 
         vec![
-            AssetDepositMethod {
+            AssetCryptoDepositMethod {
              asset_id: "BTC",
-             deposit_methods: vec![DepositMethod::Native],
+             deposit_methods: vec![CryptoDepositMethod::Native],
             },
 
-        AssetDepositMethod {
+        AssetCryptoDepositMethod {
             asset_id: "ETH",
-            deposit_methods: vec![DepositMethod::Native],
+            deposit_methods: vec![CryptoDepositMethod::Native],
         },
 
-        AssetDepositMethod {
+        AssetCryptoDepositMethod {
             asset_id: "ETH",
-            deposit_methods: vec![DepositMethod::Native],
+            deposit_methods: vec![CryptoDepositMethod::Native],
         },
 
-        AssetDepositMethod {
+        AssetCryptoDepositMethod {
             asset_id: "USDT",
-            deposit_methods: vec![DepositMethod::EthErc20, DepositMethod::Trc20, DepositMethod::BinanceBep20],
+            deposit_methods: vec![CryptoDepositMethod::EthErc20, CryptoDepositMethod::Trc20, CryptoDepositMethod::BinanceBep20],
         },
 
 
-        AssetDepositMethod {
+        AssetCryptoDepositMethod {
             asset_id: "SOL",
-            deposit_methods: vec![DepositMethod::Native],
+            deposit_methods: vec![CryptoDepositMethod::Native],
         },
 
-        AssetDepositMethod {
+        AssetCryptoDepositMethod {
             asset_id: "DOT",
-            deposit_methods: vec![DepositMethod::Native],
+            deposit_methods: vec![CryptoDepositMethod::Native],
         },
 
-        AssetDepositMethod {
+        AssetCryptoDepositMethod {
             asset_id: "TRX",
-            deposit_methods: vec![DepositMethod::Native],
+            deposit_methods: vec![CryptoDepositMethod::Native],
         },
 
-        AssetDepositMethod {
+        AssetCryptoDepositMethod {
             asset_id: "LTC",
-            deposit_methods: vec![DepositMethod::Native],
+            deposit_methods: vec![CryptoDepositMethod::Native],
         },
 
-        AssetDepositMethod {
+        AssetCryptoDepositMethod {
             asset_id: "ADA",
-            deposit_methods: vec![DepositMethod::Native],
+            deposit_methods: vec![CryptoDepositMethod::Native],
         },
 
-        AssetDepositMethod {
+        AssetCryptoDepositMethod {
             asset_id: "ICP",
-            deposit_methods: vec![DepositMethod::Native],
+            deposit_methods: vec![CryptoDepositMethod::Native],
         },
 
-        AssetDepositMethod {
+        AssetCryptoDepositMethod {
             asset_id: "XRP",
-            deposit_methods: vec![DepositMethod::Native],
+            deposit_methods: vec![CryptoDepositMethod::Native],
         },
 
-        AssetDepositMethod {
+        AssetCryptoDepositMethod {
             asset_id: "XLM",
-            deposit_methods: vec![DepositMethod::Native],
+            deposit_methods: vec![CryptoDepositMethod::Native],
         },
 
-        AssetDepositMethod {
+        AssetCryptoDepositMethod {
             asset_id: "HBAR",
-            deposit_methods: vec![DepositMethod::Native],
+            deposit_methods: vec![CryptoDepositMethod::Native],
         },
 
-        AssetDepositMethod {
+        AssetCryptoDepositMethod {
             asset_id: "BNB",
-            deposit_methods: vec![DepositMethod::Native],
+            deposit_methods: vec![CryptoDepositMethod::Native],
         },
 
-        AssetDepositMethod {
+        AssetCryptoDepositMethod {
             asset_id: "AVAX",
-            deposit_methods: vec![DepositMethod::Native],
+            deposit_methods: vec![CryptoDepositMethod::Native],
         }
 
         ]
