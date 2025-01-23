7f8b3888df86c737fb9364241f1f5bb1ca084405
amigin <amigin@gmail.com>
2025-01-23 18:14:13 +0200

    Improvements

--- src/crypto_deposit_withdraw/get_deposit_withdraw_name.rs
+++ src/crypto_deposit_withdraw/get_deposit_withdraw_name.rs
@@ -1,14 +1,18 @@
 use crate::{BNB_ID, ETH_ID, TRX_ID};
 
-pub fn get_deposit_withdraw_name<'s>(asset_id: &'s str, network_id: &'s str) -> &'s str {
+pub fn get_deposit_withdraw_name<'s>(
+    asset_id: &'s str,
+    asset_id_name: &'s str,
+    network_id: &'s str,
+) -> &'s str {
     if asset_id == network_id {
-        return asset_id;
+        return asset_id_name;
     }
 
     match network_id {
         ETH_ID => "Ethereum (ERC20)",
         TRX_ID => "Tron (TRC20)",
         BNB_ID => "Binance Smart Chain (BEP20)",
-        _ => asset_id,
+        _ => network_id,
     }
 }
